be192f1729b0043627dd5a228c22675e1c4b2be9
Özge Kocaoğlu <38084944+OzgeKocaoglu@users.noreply.github.com>
2020-08-29 21:27:43 +0300

    Sapka fixed.

--- commands/secmensapka.js
+++ commands/secmensapka.js
@@ -2,9 +2,19 @@ module.exports = {
 	name: 'secmensapka',
 	description: 'Seçmen şapka komutu!',
 	execute(message, args) {
-        var rand = Math.floor((Math.random() * 4) + 1);
+        var rand = Math.floor((Math.random() * 4) + 1); 
         if(rand == 1){
             message.channel.send('Slytherin!');
+            let role = message.guild.roles.cache.find(r => r.name === "Slytherin");
+            console.log(role);
+            let member = message.mentions.members.first();
+            if(message.sender.roles.cache.some(r => r.name === role)){
+                message.channel.send("Zaten Slytherin'sin.");
+            }
+            else{
+                member.roles.add(role).catch(message.error);
+            }
+
         }
         else if(rand == 2){
             message.channel.send('Hufflepuff!');
@@ -15,5 +25,5 @@ module.exports = {
         else if(rand == 4){
             message.channel.send('Ravenclaw');
         }
-	},
+	}
 };
