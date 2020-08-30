bcd36aa7eb8003d03fd9f0f160bbb6f6cfb50697
Özge Kocaoğlu <38084944+OzgeKocaoglu@users.noreply.github.com>
2020-08-30 23:49:19 +0300

    Secmensapka fixed.

--- commands/secmensapka.js
+++ commands/secmensapka.js
@@ -5,16 +5,6 @@ module.exports = {
         var rand = Math.floor((Math.random() * 4) + 1); 
         if(rand == 1){
             message.channel.send('Slytherin!');
-            let role = message.guild.roles.cache.find(r => r.name === "Slytherin");
-            console.log(role);
-            let member = message.mentions.members.first();
-            if(message.sender.roles.cache.some(r => r.name === role)){
-                message.channel.send("Zaten Slytherin'sin.");
-            }
-            else{
-                member.roles.add(role).catch(message.error);
-            }
-
         }
         else if(rand == 2){
             message.channel.send('Hufflepuff!');
