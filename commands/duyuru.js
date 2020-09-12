24c8278e3f931e116f621fe5438a5fb8b24319be
Özge Kocaoğlu <38084944+OzgeKocaoglu@users.noreply.github.com>
2020-09-12 21:27:13 +0300

    Anno Command

--- commands/duyuru.js
+++ commands/duyuru.js
@@ -6,14 +6,11 @@ module.exports.run = async (client, message, args) =>{
         if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("Buna iznin yok fani, kaybol gözümün önünden!")
 
         let argresult;
-        const mChannel = client.channels.get("id", "749844028648062986")
+        let mChannel = client.channels.cache.get(`749844028648062986`)
+        message.delete()
         if(mChannel){
             argresult = args.slice(1).join(" ")
-            const botemded = new Discord.MessageEmbed()
-            .setDescription(argresult)
-            .setTitle('Duyuru')
-            .setColor("#b3b330")
-            mChannel.send(botemded)
+            mChannel.send(argresult)
         }
         else{
             argresult = args.join(" ")
