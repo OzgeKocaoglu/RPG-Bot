a63d56937619e8c03b45338ec0988376533d2c78
Özge Kocaoğlu <38084944+OzgeKocaoglu@users.noreply.github.com>
2020-09-13 22:53:15 +0300

    Kayıt Command

---
+++ commands/kayıt.js
@@ -0,0 +1,37 @@
+
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) =>{
+
+    if(message.member.roles.cache.some(r => r.name === "Kamp Kılavuzu") || message.member.roles.cache.some(r => r.name === "Rol Denetmeni") || message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])){
+
+        var user = message.mentions.users.first();
+        if(!user) return message.reply("Böyle bir kullanıcı yok.");
+
+        var member;
+        try{
+            member = await message.guild.members.fetch(user);
+        }catch{
+            member = null;
+        }
+
+        if(!member) return message.reply("Sunucuda böyle bir kullanıcı yok.");
+        member.roles.add("754300516770775143")
+        member.roles.add("751059991833608294")
+        return message.reply("Kayıt başarıyla tamamlandı.");
+        
+    }
+    else{
+        return message.channel.send("Buna iznin yok fani, kaybol gözümün önünden!");
+    }
+        
+
+};
+
+
+exports.help = {
+    name: 'kayıt',
+    aliases: ['kayıt'],
+    description: 'kayıt',
+
+  }
